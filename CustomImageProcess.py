6ea6dd46818e5ec420f83690eb6fde583d04a119
Matt Kierans <mattkierans@gmail.com>
2016-05-25 22:24:58 -0700

    Update CustomImageProcess.py

--- CustomImageProcess.py
+++ CustomImageProcess.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 
 Documentation for this class : https://github.com/fchollet/keras/blob/master/keras/preprocessing/image.py
@@ -10,11 +9,11 @@ from keras.preprocessing.image import *
 import numpy as np
 
 class CustomImageProcess(ImageDataGenerator):
-    flip_indices = [
-        (0, 2), (1, 3),
-        (4, 8), (5, 9), (6, 10), (7, 11),
-        (12, 16), (13, 17), (14, 18), (15, 19),
-        (22, 24), (23, 25)]
+    
+    def __init__(self, flip_indices, horizontal_flip):
+        ImageDataGenerator.__init__(self, horizontal_flip)
+        self.flip_indices = flip_indices    
+    
     def next(self):
         # for python 2.x.
         # Keeps under lock only the mechanism which advances
@@ -30,16 +29,9 @@ class CustomImageProcess(ImageDataGenerator):
             x = self.X[j]
             Y = self.y[j]
             x, Y = self.random_transform(x.astype('float32'), Y.astype('float32'))
-#            x = self.standardize(x)
             bX[i] = x
             bY[i] = Y
-        if self.save_to_dir:
-            for i in range(current_batch_size):
-                img = array_to_img(bX[i], self.dim_ordering, scale=True)
-                fname = '{prefix}_{index}.{format}'.format(prefix=self.save_prefix,
-                                                           index=current_index + i,
-                                                           format=self.save_format)
-                img.save(os.path.join(self.save_to_dir, fname))
+            
         return (bX, bY)
         
     def random_transform(self, x, Y):
@@ -57,5 +49,4 @@ class CustomImageProcess(ImageDataGenerator):
                 for a, b in self.flip_indices:
 #                    print a, 'swaps with', b
                     Y[a], Y[b] = Y[b], Y[a]
-
         return x, Y
